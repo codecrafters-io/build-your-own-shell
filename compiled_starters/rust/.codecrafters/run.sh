f7631a97530b6626596f2326b034793e8f8ebbf4
Rafey Ahmad <rafeyahmad@protonmail.com>
2024-07-04 22:01:03 +0530

    fix typos

--- compiled_starters/rust/.codecrafters/run.sh
+++ compiled_starters/rust/.codecrafters/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# This script is used to compile your program on CodeCrafters
+# This script is used to run your program on CodeCrafters
 # 
 # This runs after .codecrafters/compile.sh
 #
