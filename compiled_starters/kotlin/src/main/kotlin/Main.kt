11e472b87ffce0462d211bb08baa2f613b4d0a0e
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-12 15:30:47 +0100

    add kotlin

---
+++ compiled_starters/kotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+import java.util.Scanner
+
+fun main() {
+    // Uncomment this block to pass the first stage
+    // print("$ ")
+
+    readln() // Wait for user input
+}
---
+++ solutions/kotlin/01-oo8/code/src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+import java.util.Scanner
+
+fun main() {
+    print("$ ")
+
+    readln() // Wait for user input
+}
