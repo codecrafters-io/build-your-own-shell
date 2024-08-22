e446f87d69df6cff9a5ee488611889df46c54722
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-08-22 20:02:53 +0100

    Upgrade Rust

--- solutions/rust/01-oo8/code/.codecrafters/run.sh
+++ solutions/rust/01-oo8/code/.codecrafters/run.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
 #
 # This script is used to run your program on CodeCrafters
-# 
+#
 # This runs after .codecrafters/compile.sh
 #
 # Learn more: https://codecrafters.io/program-interface
 
-# Exit early if any commands fail
-set -e
-
-exec "/tmp/codecrafters-shell-target/release/shell-starter-rust" "$@"
+exec /tmp/codecrafters-build-shell-rust/release/codecrafters-shell "$@"
