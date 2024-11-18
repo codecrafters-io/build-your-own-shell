3cf05145146c8157f8756b27d3f403f3ea354ac4
Ryan Gang <ryan-gg@outlook.com>
2024-11-18 23:09:16 +0530

    feat: update main.cs

--- starter_templates/csharp/code/src/main.cs
+++ starter_templates/csharp/code/src/main.cs
@@ -2,10 +2,7 @@ using System.Net;
 using System.Net.Sockets;
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
-// Console.WriteLine("Logs from your program will appear here!");
+Console.WriteLine("Logs from your program will appear here!");
 
-// Uncomment this line to pass the first stage
-// Console.Write("$ ");
-
-// Wait for user input
-Console.ReadLine();
+// Uncomment this block to pass the first stage
+// Console.WriteLine("Implement starter code");
