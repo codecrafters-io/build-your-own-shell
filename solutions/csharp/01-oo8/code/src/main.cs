a09e548b4c8c1b088068cce94446e38f64f5b6ff
Ryan Gang <ryan-gg@outlook.com>
2024-11-20 10:07:27 +0530

    feat: add top level statements in main.cs instead of main method

--- solutions/csharp/01-oo8/code/src/main.cs
+++ solutions/csharp/01-oo8/code/src/main.cs
@@ -1,16 +1,9 @@
 using System.Net;
 using System.Net.Sockets;
+// You can use print statements as follows for debugging, they'll be visible when running tests.
+// Console.WriteLine("Logs from your program will appear here!");
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        // Console.WriteLine("Logs from your program will appear here!");
+Console.Write("$ ");
 
-        Console.Write("$ ");
-        
-        // Wait for user input
-        Console.ReadLine();
-    }
-}
+// Wait for user input
+Console.ReadLine();
