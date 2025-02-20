6dabcc106dabcb915117c244718c52029574a5df
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-02-20 20:09:41 +0800

    refactor: Migrate C starter template to CMake and C23

--- solutions/c/01-oo8/code/your_program.sh
+++ solutions/c/01-oo8/code/your_program.sh
@@ -14,11 +14,12 @@ set -e # Exit early if any commands fail
 # - Edit .codecrafters/compile.sh to change how your program compiles remotely
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
-  gcc app/*.c -o /tmp/shell-target
+  cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+  cmake --build ./build
 )
 
 # Copied from .codecrafters/run.sh
 #
 # - Edit this to change how your program runs locally
 # - Edit .codecrafters/run.sh to change how your program runs remotely
-exec /tmp/shell-target "$@"
+exec $(dirname $0)/build/shell "$@"
--- starter_templates/c/code/src/main.c
+++ starter_templates/c/code/src/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
+int main(int argc, char *argv[]) {
   // Flush after every printf
   setbuf(stdout, NULL);
 
-  printf("$ ");
+  // Uncomment this block to pass the first stage
+  // printf("$ ");
 
   // Wait for user input
   char input[100];
