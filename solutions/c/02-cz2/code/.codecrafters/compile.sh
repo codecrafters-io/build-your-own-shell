c7fb9de0823f072500aaa612172f1f711b837336
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-02-20 20:47:40 +0800

    refactor: Update CodeCrafters compilation and run scripts to use CMake

--- solutions/c/02-cz2/code/.codecrafters/compile.sh
+++ solutions/c/02-cz2/code/.codecrafters/compile.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #
 # This script is used to compile your program on CodeCrafters
-# 
+#
 # This runs before .codecrafters/run.sh
 #
 # Learn more: https://codecrafters.io/program-interface
 
-# Exit early if any commands fail
-set -e
+set -e # Exit on failure
 
-gcc app/*.c -o /tmp/shell-target
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+cmake --build ./build
