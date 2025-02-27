6378ea36b6017018d0c1b2e505203abd6ee5eb56
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-02-27 20:43:01 +0900

    refactor: Update Go starter template to use Go 1.24 and move main.go to app directory

--- solutions/go/02-cz2/code/.codecrafters/compile.sh
+++ solutions/go/02-cz2/code/.codecrafters/compile.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 #
 # This script is used to compile your program on CodeCrafters
-# 
+#
 # This runs before .codecrafters/run.sh
 #
 # Learn more: https://codecrafters.io/program-interface
 
-# Exit early if any commands fail
-set -e
+set -e # Exit on failure
 
-go build -o /tmp/shell-target cmd/myshell/*.go
+go build -o /tmp/codecrafters-build-shell-go app/*.go
