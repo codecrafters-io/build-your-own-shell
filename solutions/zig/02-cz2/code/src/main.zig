d05c2d3b1c783fb246c2d2b074bf61afb8328738
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-09-11 07:07:20 +0800

    Update Zig version to 0.15 and adjust build configurations
    - Updated minimum Zig version in build.zig.zon and codecrafters.yml files to 0.15.1.
    - Modified build.zig to use the new module creation method.
    - Updated README.md and your_program.sh to reflect the new Zig version.
    - Adjusted run.sh scripts to ensure proper execution of the Zig application.
    - Added Dockerfile for Zig 0.15 and updated starter templates accordingly.

--- solutions/zig/02-cz2/code/src/main.zig
+++ solutions/zig/02-cz2/code/src/main.zig
@@ -1,10 +1,13 @@
 const std = @import("std");
 var stdin = std.fs.File.stdin().readerStreaming(&.{});
+var stdout = std.fs.File.stdout().writerStreaming(&.{});
 
 pub fn main() !void {
     std.debug.print("$ ", .{});
 
     var input_buffer: [1024]u8 = undefined;
     const input_len = try stdin.read(&input_buffer);
-    _ = input_buffer[0 .. input_len - 1]; // TODO: Handle user input
+    const input_slice = input_buffer[0 .. input_len - 1];
+
+    try stdout.interface.print("{s}: command not found\n", .{input_slice});
 }
