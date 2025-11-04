047c4f871e4f70980c26ab6266397ded6db8ef7b
TropicolX <jacobsbusayo@gmail.com>
2025-11-04 18:26:44 +0100

    updates based on updated zig code

--- solutions/zig/02-cz2/code/src/main.zig
+++ solutions/zig/02-cz2/code/src/main.zig
@@ -1,20 +1,15 @@
 const std = @import("std");
 
-var in_buf: [1024]u8 = undefined;
-var out_buf: [1024]u8 = undefined;
+var stdin_buffer: [4096]u8 = undefined;
+var stdin_reader = std.fs.File.stdin().readerStreaming(&stdin_buffer);
+const stdin = &stdin_reader.interface;
 
-var reader_impl = std.fs.File.stdin().reader(&in_buf);
-var writer_impl = std.fs.File.stdout().writer(&out_buf);
-
-const stdin = &reader_impl.interface;
-const stdout = &writer_impl.interface;
+var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
+const stdout = &stdout_writer.interface;
 
 pub fn main() !void {
     try stdout.print("$ ", .{});
-    try stdout.flush();
 
-    const command = try stdin.takeDelimiterExclusive('\n');
-
-    try stdout.print("{s}: command not found\n", .{command});
-    try stdout.flush();
+    const command = try stdin.takeDelimiter('\n');
+    try stdout.print("{s}: command not found\n", .{command.?});
 }
