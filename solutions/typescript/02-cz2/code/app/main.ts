8fafc82a58ac8d0077d04559fc074e9d90e2ca10
TropicolX <jacobsbusayo@gmail.com>
2025-11-04 21:07:32 +0100

    update single quotes to double

--- solutions/typescript/02-cz2/code/app/main.ts
+++ solutions/typescript/02-cz2/code/app/main.ts
@@ -1,11 +1,11 @@
-import { createInterface } from 'readline';
+import { createInterface } from "readline";
 
 const rl = createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-rl.question('$ ', (command) => {
+rl.question("$ ", (command) => {
   console.log(`${command}: command not found`);
   rl.close();
 });
