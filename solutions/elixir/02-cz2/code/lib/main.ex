d044d5fb73ace88f307075d7128b99850f8dba0d
TropicolX <jacobsbusayo@gmail.com>
2025-11-20 12:56:27 +0100

    simplify elixir solution

--- solutions/elixir/02-cz2/code/lib/main.ex
+++ solutions/elixir/02-cz2/code/lib/main.ex
@@ -2,13 +2,7 @@ defmodule CLI do
   def main(_args) do
     IO.write("$ ")
 
-    case IO.gets("") do
-      nil ->
-        :ok
-
-      command ->
-        command = String.trim(command)
-        IO.puts("#{command}: command not found")
-    end
+    command = IO.gets("") |> String.trim()
+    IO.puts("#{command}: command not found")
   end
 end
