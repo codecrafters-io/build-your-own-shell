8d794f62ff1b7a8031960e4ddb79b56ba317b5ba
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-11-28 12:32:02 +0900

    Refactor output statement in main.go to use fmt.Print instead of fmt.Fprint. Remove outdated explanation.md file.

--- solutions/go/01-oo8/code/app/main.go
+++ solutions/go/01-oo8/code/app/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
-// Ensures gofmt doesn't remove the "fmt" and "os" imports in stage 1 (feel free to remove this!)
-var _ = fmt.Fprint
-var _ = os.Stdout
+// Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
+var _ = fmt.Print
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
+	fmt.Print("$ ")
 }
