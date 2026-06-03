023ca83111396c0cb96e57e9c00e0e2ccb5b0995
Andy Li <1450947+andy1li@users.noreply.github.com>
2026-06-04 05:42:13 +0900

    Add C# starter project and update configurations to .NET 10.0
    - Created new C# project and solution files for CodeCrafters.Shell.
    - Updated buildpack version in configuration files to dotnet-10.0.
    - Modified README and script files to reflect changes in entry point and required .NET version.
    - Added initial Program.cs file with debugging output.
    - Updated Dockerfile and compile/run scripts to use the new project structure.

--- solutions/csharp/03-ff0/code/.codecrafters/compile.sh
+++ solutions/csharp/03-ff0/code/.codecrafters/compile.sh
@@ -8,4 +8,4 @@
 
 set -e # Exit on failure
 
-dotnet build --configuration Release --output /tmp/codecrafters-build-csharp codecrafters-shell.csproj
+dotnet build --configuration Release --output /tmp/codecrafters-build-csharp CodeCrafters.Shell.csproj
